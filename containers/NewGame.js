2be628d0c6b44eb4cd0d197d37f98be67fcf4b58
Caleb Atwood <calebatwd@gmail.com>
2018-03-03 15:14:31 -0500

    Rename new game container.

---
+++ containers/NewGame.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+class NewGame extends Component {
+  render() {
+    <Text>Huge</Text>;
+  }
+}
+
+export default NewGame;
--- containers/NewGameScreen.js
+++
@@ -1,5 +0,0 @@
-import React, {Component} from 'react';
-
-class NewGameScreen extends Component {}
-
-export default NewGameScreen;
