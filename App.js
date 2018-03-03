5cc24dbeb5bcb864f12d8f6263b84ad98d49d803
Caleb Atwood <calebatwd@gmail.com>
2018-03-03 15:24:56 -0500

    Move title component out of app.js. Install react-native-router

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Font} from 'expo';
 import {StyleSheet, Text, View, TouchableHighlight} from 'react-native';
+import {NativeRouter, Route, Link} from 'react-router-native';
+
+import Title from './components/Title';
 
 import * as firebase from 'firebase';
-
-import colors from './resources/colors.json';
 
 // Initialize Firebase
 const firebaseConfig = {
@@ -33,29 +34,13 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        {this.state.fontLoaded && <Text style={styles.title}>Maniquest Questiny</Text>}
-
-        <TouchableHighlight
-          style={styles.button}
-          onPress={() => {
-            console.log('New game pressed');
-          }}
-          underlayColor={colors.slate}
-        >
-          <Text style={styles.buttonText}>New Game</Text>
-        </TouchableHighlight>
-
-        <TouchableHighlight
-          style={styles.button}
-          onPress={() => {
-            console.log('Join game pressed');
-          }}
-          underlayColor={colors.slate}
-        >
-          <Text style={styles.buttonText}>Join Game</Text>
-        </TouchableHighlight>
-      </View>
+      <NativeRouter>
+        {this.state.fontLoaded && (
+          <View style={styles.container}>
+            <Route exact path="/" component={Title} />
+          </View>
+        )}
+      </NativeRouter>
     );
   }
 }
@@ -63,29 +48,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: 10,
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: colors.orange,
-    padding: 10,
-    margin: 16,
-    width: 200,
-    height: 60,
-    justifyContent: 'center',
-  },
-  buttonText: {
-    fontSize: 20,
-    color: '#FFF',
-  },
-  title: {
-    fontSize: 44,
-    fontFamily: 'SpaceMonoBold',
-    marginBottom: 40,
-    textAlign: 'center',
-    color: colors.purple,
+    backgroundColor: colors.white,
   },
 });
---
+++ components/Title.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {StyleSheet, Text, View, TouchableHighlight} from 'react-native';
+import {NativeRouter, Route, Link} from 'react-router-native';
+
+import colors from '../resources/colors.json';
+
+export default class Title extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Maniquest Questiny</Text>
+
+        <TouchableHighlight
+          style={styles.button}
+          onPress={() => {
+            console.log('New game pressed');
+          }}
+          underlayColor={colors.slate}
+        >
+          <Text style={styles.buttonText}>New Game</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight
+          style={styles.button}
+          onPress={() => {
+            console.log('Join game pressed');
+          }}
+          underlayColor={colors.slate}
+        >
+          <Text style={styles.buttonText}>Join Game</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: colors.orange,
+    padding: 10,
+    margin: 16,
+    width: 200,
+    height: 60,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#FFF',
+  },
+  title: {
+    fontSize: 44,
+    fontFamily: 'SpaceMonoBold',
+    marginBottom: 40,
+    textAlign: 'center',
+    color: colors.purple,
+  },
+});
