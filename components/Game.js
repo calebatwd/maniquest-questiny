9445a79132d70b33925675fd60a942683f0c3d35
Caleb Atwood <calebatwd@gmail.com>
2018-03-04 09:57:37 -0500

    Add dsstore to gitignore and modify basic of game.js

--- components/Game.js
+++ components/Game.js
@@ -6,11 +6,39 @@ import * as firebase from 'firebase';
 import colors from '../resources/colors.json';
 
 class Game extends Component {
+  state = {};
+
+  componentWillMount() {
+    this.gameId = this.props.match.params.gameId;
+
+    const gameRef = firebase.database().ref(`games/${this.gameId}`);
+    gameRef.on(
+      'child_added',
+      (snap) => {
+        const name = snap.val().name;
+        this.setState((prevState) => {
+          return {
+            players: [...prevState.players, {key: name}],
+          };
+        });
+      },
+      (error) => {
+        console.log('Failed to fetch players. ' + error);
+      }
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <View>
           <Text>Top</Text>
+        </View>
+        <View>
+          <Text>Other players hand</Text>
+        </View>
+        <View>
+          <Text>Your hand</Text>
         </View>
         <Text style={styles.title}>Game</Text>
       </View>
